fe238008139165bf87f8aa7fb801681ad38a719a
Magdelena <magdelenagavrilova@gmail.com>
2022-04-26 14:48:05 +0200

    changed

--- Task-Manager/app.js
+++ Task-Manager/app.js
@@ -19,13 +19,13 @@ app.use(express.json())
 app.use("/api/v1/tasks", tasks)
 
 
-const port = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 
 
 const start = async () => {
     try {
        await connectDB()
-       app.listen(port, () => console.log(`server is listening on port ${port}...`))
+       app.listen(PORT, () => console.log(`server is listening on port ${port}...`))
     } catch (error) {
           console.log(error)
     }
